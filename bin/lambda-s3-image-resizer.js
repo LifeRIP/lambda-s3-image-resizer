f87f0e8d0b2038b4e7c66ecf67459a0cb9fed702
Joan Manuel Jaramillo Avila <89425013+LifeRIP@users.noreply.github.com>
2025-11-07 11:09:10 -0500

    feat: add React frontend to support image resizer application
    Introduces a React frontend using Vite for the image resizer application.
    
    Includes:
    - A new CDK stack for deploying the frontend alongside the backend.
    - A basic React app setup with ESLint configuration, CSS styles, and example components.
    - Scripts for development, build, linting, and preview in `package.json`.

--- bin/lambda-s3-image-resizer.js
+++ bin/lambda-s3-image-resizer.js
@@ -5,6 +5,8 @@ const cdk = require("aws-cdk-lib");
 const {
   LambdaS3ImageResizerStack,
 } = require("../lib/lambda-s3-image-resizer-stack");
+const { FrontendStack } = require("../lib/frontend-stack");
 
 const app = new cdk.App();
 new LambdaS3ImageResizerStack(app, "LambdaS3ImageResizerStack");
+new FrontendStack(app, "FrontendStack");
